488f2c6ac13d05b2e5fd1aa0da4ef03dfa6e4dc6
Joyal <jpaul11@binghamton.edu>
2025-04-21 11:29:51 -0400

    updated

--- coursicle/grabing.py
+++ coursicle/grabing.py
@@ -1,8 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 '''-----------------------------------------Step One - Getting to the links--------------------------------------------'''
 def fetch_professors(course, course_number):
+    '''
+    Function takes a course abbreviation and number to find course page with professors
+    args: course, course_number
+    return: url
+    '''
     course = course.upper()
     url = f"https://www.coursicle.com/binghamton/courses/{course}/{course_number}"
     return url
@@ -10,6 +16,11 @@ def fetch_professors(course, course_number):
 url = fetch_professors(input("Subject: "), input("Course Number: "))      # Change when necessary
 
 def write_info():
+    '''
+    writes everything from the url into a info.html file
+    args: None
+    return: None
+    '''
     response = requests.get(url)
     with open("info.html", "w", encoding="utf-8") as f:
         f.write(response.text)
@@ -22,6 +33,7 @@ with open("info.html", "r", encoding="utf-8") as f:
 
 soup = BeautifulSoup(content, "html.parser")
 div = soup.find_all(attrs={"class": "professorLink"})
+# List for each professors info
 professor_links = []
 professor_names = []
 score_list = []
@@ -34,10 +46,16 @@ for links in div:
 
 number = 0
 def write_prof(list_index):
+    '''
+    Gets an individual professor's info and writes it into prof_info.html
+    args: list_index
+    return: None
+    '''
     prof_response = requests.get(professor_links[list_index])
     with open("prof_info.html", "w", encoding="utf-8") as f:
         f.write(prof_response.text)
 
+# Goes through each professor link and pulss both name and score
 for _ in range(len(professor_links)):
     write_prof(list_index=number)
     with open("prof_info.html", "r", encoding="utf-8") as f:
@@ -51,6 +69,7 @@ for _ in range(len(professor_links)):
     else: 
         score_list.append("Score not given")
     number += 1
+
 
 for rows in range(len(professor_names)):
     print(f'{professor_links[rows]} <> {professor_names[rows]} : {score_list[rows]}')
